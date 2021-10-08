15906967e6fb91badaa9f75c8fcf517a158cc0d1
Luis Alfredo Elias Humar <luiseliashumar@gmail.com>
2021-10-08 10:55:28 -0700

    page wrapper

---
+++ lib/presentation/wrapper/bloc/wrapper_state.dart
@@ -0,0 +1,6 @@
+part of 'wrapper_bloc.dart';
+
+class IndexChangedState{
+  IndexChangedState({required this.currentIndex});
+  final int currentIndex;
+}
--- lib/presentation/wrapper/wrapper_page.dart
+++ lib/presentation/wrapper/wrapper_page.dart
@@ -1,69 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:lyrics_app/presentation/home/home_page.dart';
 import 'package:lyrics_app/presentation/shared/custom_navigation_bar.dart';
+import 'package:lyrics_app/presentation/wrapper/bloc/wrapper_bloc.dart';
 import 'package:lyrics_app/styles.dart';
 
 class WrapperPage extends StatelessWidget {
-  const WrapperPage({ Key? key }) : super(key: key);
+  const WrapperPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButtonLocation: 
-        FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: CustomNavigationBar(
-          index: 0,
-          onIndexSelected: (index){
-           
-          },
-        ),
-        floatingActionButton: Container(
-          padding: const EdgeInsets.all(3.0),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.circle
+    return BlocProvider(
+      create: (_) => WrapperBloc(),
+      child: WrapperPageUI(),
+    );
+  }
+}
+
+class WrapperPageUI extends StatelessWidget {
+  const WrapperPageUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final _bloc = BlocProvider.of<WrapperBloc>(context);
+
+    return BlocBuilder<WrapperBloc, IndexChangedState>(
+      builder: (context, state) {
+        return Scaffold(
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: CustomNavigationBar(
+            index: state.currentIndex,
+            onIndexSelected: (index) {
+              _bloc.add(IndexChangedEvent(index: index));
+            },
           ),
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: blueDark
-            ),
-            child: FloatingActionButton(
-              onPressed: (){
-                //_bloc.add(IndexChangedEvent(index: 2));
-              },
-              elevation: 0,
-              highlightElevation: 0,
-              foregroundColor: Colors.white,
-              splashColor: Colors.transparent,
-              backgroundColor: Colors.transparent,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: SvgPicture.asset(
-                  'assets/search.svg',
-                  color: Colors.white,
+          floatingActionButton: Container(
+            padding: const EdgeInsets.all(3.0),
+            decoration: const BoxDecoration(
+                color: Colors.white, shape: BoxShape.circle),
+            child: Container(
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: blueDark),
+              child: FloatingActionButton(
+                onPressed: () {
+                  _bloc.add(IndexChangedEvent(index: 2));
+                },
+                elevation: 0,
+                highlightElevation: 0,
+                foregroundColor: Colors.white,
+                splashColor: Colors.transparent,
+                backgroundColor: Colors.transparent,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: SvgPicture.asset(
+                    'assets/search.svg',
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: IndexedStack(
-                index: 0,
+          body: Column(
+            children: [
+              Expanded(
+                  child: IndexedStack(
+                index: state.currentIndex,
                 children: [
-                  const HomePage(),
-                  Container(),
-                  Container(),
-                  Container(),
-                  Container()
+                  Container(
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    color: Colors.red,
+                  ),
+                  Container(
+                    color: Colors.green,
+                  ),
+                  Container(
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                  )
                 ],
-              )
-            ),
-          ],
-        ),
-      );
+              )),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
