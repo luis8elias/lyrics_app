78afb22bec3a9a628dd877a6f53cbbb2eb37a05f
krauser182 <ivan_ale03@hotmail.com>
2021-11-02 11:49:56 -0700

    Update song_list.dart

---
+++ lib/presentation/lyric/song_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SongList extends StatelessWidget {
+  const SongList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>['A', 'B', 'C', 'D'];
+    final List<int> colorCodes = <int>[600, 500, 100];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Canciones",
+            style: TextStyle(color: Colors.white, fontSize: 28)),
+        leading: GestureDetector(
+          onTap: () {
+            print('si jala el ontap salu2');
+          },
+          child: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color(0xFF2F80ED),
+        elevation: 0,
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(7),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 80,
+            color: Colors.cyan[100],
+            //Todo @ aqui entra el texto de los contenedores  de las canciones
+            child: Container(),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      ),
+    );
+  }
+}
