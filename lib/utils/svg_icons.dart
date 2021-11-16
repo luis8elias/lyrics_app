e8732ce9afdeb06aa1dfedd99551335cdbc1b064
Raul-Reyes16 <a18050025@unisierra.edu.mx>
2021-11-16 16:22:43 -0700

    Iconos

--- lib/utils/svg_icons.dart
+++ lib/utils/svg_icons.dart
@@ -1,14 +1,14 @@
 class SvgIcons {
-  static final String arrowDown = 'assets/arrow-left.svg';
+  static final String arrowDown = 'assets/arrow-down.svg';
   static final String arrowLeft = 'assets/arrow-left.svg';
-  static final String dots = 'assets/arrow-left.svg';
-  static final String home = 'assets/arrow-left.svg';
-  static final String musicNote1 = 'assets/arrow-left.svg';
-  static final String musicNoteSolid = 'assets/arrow-left.svg';
-  static final String pencil = 'assets/arrow-left.svg';
-  static final String save = 'assets/arrow-left.svg';
-  static final String searchNormal = 'assets/arrow-left.svg';
-  static final String size = 'assets/arrow-left.svg';
-  static final String trash = 'assets/arrow-left.svg';
-  static final String user = 'assets/arrow-left.svg';
+  static final String dots = 'assets/dots.svg';
+  static final String home = 'assets/home.svg';
+  static final String musicNote1 = 'assets/music-note-1.svg';
+  static final String musicNoteSolid = 'assets/music-note-solid.svg';
+  static final String pencil = 'assets/pencil.svg';
+  static final String save = 'assets/save.svg';
+  static final String searchNormal = 'assets/search-normal.svg';
+  static final String size = 'assets/size.svg';
+  static final String trash = 'assets/trash.svg';
+  static final String user = 'assets/user.svg';
 }
