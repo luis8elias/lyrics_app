f4811a0b4ff24afd8a224fc7ba2f0b3daeb0171c
krauser182 <ivan_ale03@hotmail.com>
2021-11-22 13:54:03 -0700

    commit to push genre

---
+++ lib/presentation/group/list/bloc/lyrics_list_state.dart
@@ -0,0 +1,25 @@
+part of 'lyrics_list_bloc.dart';
+
+@immutable
+abstract class LyricsListState {}
+
+class LyricsListInitial extends LyricsListState {}
+
+
+class LoadingData extends LyricsListState{}
+
+class DataLoaded extends LyricsListState{
+  final List<Lyric> lyrics;
+  DataLoaded({required this.lyrics});
+}
+
+class LyricDeleted extends LyricsListState{
+  final String message;
+
+  LyricDeleted({required this.message});
+}
+
+class LyricNotDeleted extends LyricsListState{
+  final String message;
+  LyricNotDeleted({required this.message});
+}
