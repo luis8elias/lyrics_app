26337276719690233ab3c8bda8f28d91d355482a
Raul-Reyes16 <a18050025@unisierra.edu.mx>
2021-11-22 17:14:19 -0700

    Iconos 2

--- lib/utils/svg_icons.dart
+++ lib/utils/svg_icons.dart
@@ -2,13 +2,13 @@ class SvgIcons {
   static final String arrowDown = 'assets/arrow-down.svg';
   static final String arrowLeft = 'assets/arrow-left.svg';
   static final String dots = 'assets/dots.svg';
-  static final String home = 'assets/home.svg';
-  static final String musicNote1 = 'assets/music-note-1.svg';
+  static final String home = 'assets/home-bold.svg';
+  static final String musicNote1 = 'assets/music-note-bold-1.svg';
   static final String musicNoteSolid = 'assets/music-note-solid.svg';
   static final String pencil = 'assets/pencil.svg';
   static final String save = 'assets/save.svg';
   static final String searchNormal = 'assets/search-normal.svg';
-  static final String size = 'assets/size.svg';
+  static final String size = 'assets/size-bold.svg';
   static final String trash = 'assets/trash.svg';
-  static final String user = 'assets/user.svg';
+  static final String user = 'assets/user-bold.svg';
 }
