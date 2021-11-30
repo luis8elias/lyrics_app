8ce8ad9bc4bbb2ce1e32b274e0c7617306d9e529
krauser182 <ivan_ale03@hotmail.com>
2021-11-29 22:38:56 -0700

    buttons profile page done

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lyrics_app/data/repositories/dio_auth_repository.dart';
 import 'package:lyrics_app/data/repositories/dio_groups_repository.dart';
+import 'package:lyrics_app/presentation/group/create/group_page.dart';
 import 'package:lyrics_app/presentation/profile/bloc/profile_bloc.dart';
-
-
 import 'package:lyrics_app/styles.dart';
+import 'package:lyrics_app/utils/navigator.dart';
 import 'package:lyrics_app/utils/svg_icons.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -17,9 +16,8 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => ProfileBloc(
-        authRepository: DioAuthRepository(),
-        groupsRepository: DioGroupsRepository()
-      ),
+          authRepository: DioAuthRepository(),
+          groupsRepository: DioGroupsRepository()),
       child: ProfilePageUI(),
     );
   }
@@ -29,19 +27,25 @@ class ProfilePageUI extends StatelessWidget {
   const ProfilePageUI({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-
     BlocProvider.of<ProfileBloc>(context).add(LoadingProfile());
-    
+
     return BlocBuilder<ProfileBloc, ProfileState>(
       builder: (context, state) {
-
-        if (state is DataLoaded){
-
-         var arr = state.user.name.split(" ");
-         var initials = arr[0][0]+ arr[1][0];
+        if (state is DataLoaded) {
+          var arr = state.user.name.split(" ");
+          var initials = arr[0][0] + arr[1][0];
           return Scaffold(
-              backgroundColor: Theme.of(context).backgroundColor,
-              appBar: AppBar(
+            backgroundColor: Theme.of(context).backgroundColor,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: blueDark,
+              mini: true,
+              foregroundColor: Colors.white,
+              onPressed: () {
+                navigateTo(context, GroupPage());
+              },
+              child: SvgPicture.asset(SvgIcons.add, color: Colors.white),
+            ),
+            appBar: AppBar(
               automaticallyImplyLeading: false,
               leadingWidth: 40,
               centerTitle: true,
@@ -61,24 +65,37 @@ class ProfilePageUI extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: <Widget>[
                           Container(
-                              width: 50,
-                              height: 50,
-                              child: CircleAvatar(
-                                backgroundColor: ligthBlue,
-                                child: Text(
-                                  initials,
-                                  style: TextStyle(fontSize: 20),
-                                ),
+                            width: 50,
+                            height: 50,
+                            child: CircleAvatar(
+                              backgroundColor: ligthBlue,
+                              child: Text(
+                                initials,
+                                style: TextStyle(fontSize: 20),
                               ),
+                            ),
                           ),
                           Padding(
                             padding: const EdgeInsets.only(left: 25),
                             child: Text('${state.user.name}',
-                              textAlign: TextAlign.center,
-                              style: h2
+                                textAlign: TextAlign.center, style: h2),
+                          ),
+                          Expanded(child: Container()),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 15),
+                            child: IconButton(
+                              splashRadius: 25,
+                              onPressed: () {},
+                              icon: Container(
+                                width: 25,
+                                height: 25,
+                                child: Icon(
+                                  Icons.logout,
+                                  color: red,
+                                ),
+                              ),
                             ),
                           ),
-                          
                         ],
                       ),
                     ),
@@ -86,7 +103,7 @@ class ProfilePageUI extends StatelessWidget {
                   Expanded(
                     flex: 1,
                     child: Container(
-                       padding: const EdgeInsets.only(left: 25, top: 15),
+                      padding: const EdgeInsets.only(left: 25),
                       width: double.infinity,
                       child: Text(
                         "Mis Grupos",
@@ -94,137 +111,12 @@ class ProfilePageUI extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 7,
-                    child: SingleChildScrollView(
-                      child: Container(
-                      height: kBottomNavigationBarHeight * 7,
-                      child: ListView.builder(
-                        padding: const EdgeInsets.only(
-                          top: 7,
-                          left: 7,
-                          right: 7,
-                        ),
-                        itemCount: state.groups.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          bool slidableOpened = false;
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 15),
-                            child: Slidable(
-                              actionPane: SlidableDrawerActionPane(),
-                              actionExtentRatio: 0.18,
-                              child: Container(
-                                margin: EdgeInsets.symmetric(horizontal: 20.0),
-                                height: 80,
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(15)),
-                                  color: Colors.white,
-                                ),
-                                child: Container(
-                                  child: Center(
-                                    child: Builder(builder: (newContext) {
-                                      return ListTile(
-                                        onTap: () {
-                                          
-                                        },
-                                        leading: Container(
-                                            decoration: BoxDecoration(
-                                              color: blueDark,
-                                              borderRadius: BorderRadiusDirectional.all(
-                                                Radius.circular(10.0)
-                                              ),
-                                            ),
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 15, horizontal: 15),
-                                            child: SvgPicture.asset(SvgIcons.group, color: Colors.white)),
-                                        title: Text(
-                                          state.groups[index].name,
-                                          style: TextStyle(
-                                              fontSize: 18,
-                                              fontStyle: FontStyle.normal),
-                                        ),
-                                       
-                                        trailing: Container(
-                                          padding: EdgeInsetsDirectional.all(5),
-                                          width: 40,
-                                          height: 40,
-                                          child: GestureDetector(
-                                            onTap: () {
-                                              if (slidableOpened) {
-                                                Slidable.of(newContext)
-                                                    ?.close();
-                                                slidableOpened = false;
-                                              } else {
-                                                Slidable.of(newContext)?.open(
-                                                  actionType:
-                                                      SlideActionType.secondary,
-                                                );
-                                                slidableOpened = true;
-                                              }
-                                            },
-                                            child: SvgPicture.asset(
-                                              SvgIcons.dots,
-                                              color: grey
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                    }),
-                                  ),
-                                ),
-                              ),
-                              secondaryActions: <Widget>[
-                                _buildIcon(
-                                  color: Color(0xffFF4D4D),
-                                  icon: SvgIcons.trash,
-                                  onPressed: () {}
-                                ),    
-                                _buildIcon(
-                                  color: Colors.orange.shade400,
-                                  icon: SvgIcons.pencil,
-                                  onPressed: () {}
-                                ),
-                                _buildIcon(
-                                  color: green,
-                                  icon: SvgIcons.share,
-                                  onPressed: () {}
-                                ),
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    )
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: 20),
-                      width: 170,
-                      height: 120,
-                      child: ElevatedButton.icon(
-                        icon: Icon(Icons.logout),
-                        label: Text(
-                          "Cerrar Sesión",
-                          style: TextStyle(fontSize: 14),
-                        ),
-                        onPressed: () => print("it's pressed"),
-                        style: ElevatedButton.styleFrom(
-                          primary: red,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                  Expanded(flex: 4, child: SingleChildScrollView()),
                 ],
               ),
             ),
           );
-        }else{
+        } else {
           return Scaffold(
             backgroundColor: Theme.of(context).backgroundColor,
             body: Center(
@@ -239,29 +131,4 @@ class ProfilePageUI extends StatelessWidget {
       },
     );
   }
-
-  Widget _buildIcon({
-    required String icon,
-    required VoidCallback onPressed,
-    required Color color}) {
-      
-    return IconSlideAction(
-      color: Colors.transparent,
-      iconWidget: Container(
-        height: 50,
-        width: 50,
-        decoration: BoxDecoration(
-            color: color, borderRadius: BorderRadius.circular(100.0)),
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Container(
-            child: SvgPicture.asset(icon, color: Colors.white),
-          ),
-        ),
-      ),
-      onTap: onPressed,
-    );
-  }
-
-
 }
